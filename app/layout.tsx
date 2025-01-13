8fca32d82c1dfe07b8a96867ba4caad346408ac9
Emily <emily@artyom.me>
2025-01-13 22:37:01 +0100

    sha + difficulty

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
+import { Geist, Kode_Mono } from 'next/font/google'
 import './globals.css'
 
 const geistSans = Geist({
@@ -7,8 +7,8 @@ const geistSans = Geist({
   subsets: ['latin'],
 })
 
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
+const kodeMono = Kode_Mono({
+  variable: '--font-kode-mono',
   subsets: ['latin'],
 })
 
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${kodeMono.variable} antialiased`}
       >
         {children}
       </body>
